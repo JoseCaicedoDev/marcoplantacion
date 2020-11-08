bbd711d5cdd7081e19b5a22a2c8bf7a761dca808
Jcaicedo <jose.caicedo@outlook.com>
2020-11-08 12:39:01 -0500

    Primera version

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-alert("Vzla");
+const calculate = () => {
+  const surfaces = parseInt(document.getElementById("surface").value);
+  const distancePlants = parseInt(
+    document.getElementById("distancePlant").value
+  );
+  nPlant = Math.round(
+    surfaces / (Math.pow(distancePlants, 2) * Math.cos(0.523599))
+  );
+  document.getElementById("results").innerHTML = nPlant;
+};
+
+calculate();
